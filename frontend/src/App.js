6b504013ed7a0fdff4bfe8e44069d3b388efda9c
Jeremy Wood <js.wood206@gmail.com>
2021-05-31 23:07:29 -0700

    Mocked up app component to test image file input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,36 @@
 import './App.css';
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+const handleSubmit = e => {
+  e.preventDefault()
+  debugger
+
+}
+
+
+class App extends Component {
+
+  state = {
+    file: ''
+  }
+
+  handleChange = e => {
+    console.log(e.target)
+
+    debugger
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Hello
+        <form onSubmit={handleSubmit} >
+        <input onChange={event => this.handleChange(event)} type="file" name="image" accept="image/*" />
+        <input type='submit'></input>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
