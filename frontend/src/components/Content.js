b0b97b4e96b1cf2147889a663f44a81d55d70c6e
Jeremy Wood <js.wood206@gmail.com>
2021-06-10 00:01:57 -0700

    Changed post content styling

--- frontend/src/components/Content.js
+++ frontend/src/components/Content.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Content = props => {
     return (
-        <div>
+        <div className="post-content" >
             <li><b>{props.post.attributes.user}</b> - {props.post.attributes.content}</li>
             <br/>
         </div>
