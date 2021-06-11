5333d34c6b9bc24ed2423cc47114c7af86231187
Jeremy Wood <js.wood206@gmail.com>
2021-06-11 16:17:28 -0700

    Removed unnecesary code and files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,8 +10,6 @@ import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
 
-// import reportWebVitals from './reportWebVitals';
-
 const store = createStore(
   rootReducer,
   compose(
@@ -26,8 +24,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
