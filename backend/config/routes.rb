8f9d85e161853a2e233e10bee79fc5d8932b611f
Jeremy Wood <js.wood206@gmail.com>
2021-06-11 16:32:49 -0700

    Limited routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get '/auto-login' => "session#auto_login"
   post '/session' => "session#login"
-  resources :users
-  resources :posts
-  resources :comments
+  resources :users, only: :create
+  resources :posts, only: [:index, :show, :create]
+  resources :comments, only: [:index, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
