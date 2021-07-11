d6edfaa563f953f8884b7123992aeb2f051d1677
Jeremy Wood <js.wood206@gmail.com>
2021-07-11 15:41:01 -0700

    Changed some post-card css, changed some spacing.

--- frontend/src/components/Content.js
+++ frontend/src/components/Content.js
@@ -19,11 +19,12 @@ const renderHeart = props => {
 export const Content = props => {
     return (
         <div className="post-content" >
+            <br/>
             <li><b>{props.post.attributes.user}</b> - {props.post.attributes.content}</li>
+            <br/>
             <div className="likes" onClick={() => props.handleLike(props.post.id)}>
                 {renderHeart(props)}
             </div>
-            <br/>
         </div>
     )
 }
