7d6addd69cb88d7023726a188860e764a4f4e976
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2021-08-29 23:26:17 -0700

    Added basic user show page

--- frontend/src/containers/NavHeader.js
+++ frontend/src/containers/NavHeader.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Button from 'react-bootstrap/Button'
 import Badge from 'react-bootstrap/Badge'
+import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { logout } from '../actions/UserActions'
 
@@ -13,7 +14,9 @@ class NavHeader extends Component {
         if (!!this.props.user) {
             return (
                 <div className="d-flex flex-fill justify-content-between">
-                    <Badge id="user-badge" className="d-inline-flex align-items-center" pill>{this.props.user}</Badge>
+                    <NavLink to={"/users/" + this.props.user} exact>
+                        <Badge id="user-badge" className="d-inline-flex align-items-center" pill>{this.props.user}</Badge>
+                    </NavLink>
                     <Navbar.Brand href="/">PIC-SURE</Navbar.Brand>
                     <Button className="danger-button" onClick={this.logout}>LOGOUT</Button>
                 </div>
---
+++ frontend/src/containers/UserPage.js
@@ -0,0 +1,31 @@
+import { Component } from 'react'
+import { connect } from 'react-redux'
+import Post from './Post'
+
+
+// Add back button
+
+// Make back button actually use back for this page and post show page
+
+class UserPage extends Component{
+
+    renderPosts = () => {
+        return this.props.posts.map(post => <Post key={post.id} post={post}/>)
+    }
+
+    render() {
+        return (
+            <div className="post-container">
+                {this.renderPosts()}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return{
+        posts: state.postReducer.posts.filter(post => post.attributes.user === ownProps.match.url.split("/")[2])
+    }
+}
+
+export default connect(mapStateToProps)(UserPage)
