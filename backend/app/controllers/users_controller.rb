0c5d7cf4241c84da4244d4a55e8ffc3055d61aa1
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2021-08-29 22:20:50 -0700

    Added basic user show route

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -10,6 +10,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    user = User.find_by(id: [params[:id]])
+    if user 
+      posts = user.posts
+      render json: PostSerializer.new(posts)
+    else
+      render json: {status: "error", message: user.errors.full_messages[0]}
+    end
+  end
+
   private
     def user_params
       params.permit(:username, :password)
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/auto-login' => "session#auto_login"
   post '/session' => "session#login"
-  resources :users, only: :create
+  resources :users, only: [:create, :show]
   resources :posts, only: [:index, :create, :update, :destroy]
   resources :comments, only: [:index, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
